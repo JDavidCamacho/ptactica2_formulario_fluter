78a6b6c55e8b0b37de6fbb3303ecb14295b0780e
David <jdsanchez2023@incos-santacruz.edu.bo>
2025-06-03 19:54:00 -0400

    definicion de thema para el formulario

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,15 @@ class MyApp extends StatelessWidget{
     //throw UnimplementedError();
     return MaterialApp(
       title: "Formulario de Registro",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color(0XFF1E3A8A),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color(0XFF1E3A8A),
+          secondary: Color(0XFFF6B7280),
+        ),
+        useMaterial3: true,
+      ),
     )
   }
 }

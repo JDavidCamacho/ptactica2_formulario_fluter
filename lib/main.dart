47c1e7d7440a0e3d3b02d3afdfd732655a631664
David <jdsanchez2023@incos-santacruz.edu.bo>
2025-06-08 03:11:50 -0400

    formulario nombre

--- lib/main.dart
+++ lib/main.dart
@@ -31,42 +31,73 @@ class RegistrationFormsScreen extends StatelessWidget {
 
   static const Color primaryNavu = Color(0xFF1E3A6A);
   static const Color backgroundGray = Color(0xFFF9FAFB);
+  static const Color secondaryGray = Color(0xFF6B7280); // Definido para evitar error
 
   @override
   Widget build(BuildContext context) {
-   return Container(
-     padding: EdgeInsets.all(24),
-     decoration: BoxDecoration(),
-     color: Colors.white,
-     borderRadius: BorderRadius.circular(12),
-     boxShadow:[
-       BoxShadow(
-         color: secondaryGray.withOpacity(0.1),
-         blurRadius: 10,
-         offset: Offset(0, 4),
+    return Scaffold(
+      body: Center(
+    child:  Container(
+      padding: EdgeInsets.all(24),
 
-       ),
-     ],
-   ),
-    child.Column(
-    Children : [
-      Icon(Icons.confirmation_num),
-    SizedBox(height: 12,),
-    Text(
-    "datos personales"
-    style : TextStyle(
-    FontSize:24,
-    fontWeight: fontWeight.bold,
-    color: primaryNavu,
-    ),
-    ),
-    SizedBox(height: 8),
-    Text( "complete todos los campos "
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: [
+          BoxShadow(
+            color: secondaryGray.withOpacity(0.1),
+            blurRadius: 10,
+            offset: Offset(0, 4),
+          ),
+        ],
+      ),
 
-    style:TextStyle(fontSize: 14, color: backgroundGray),
+      child: Column(
+        children: [
+          Icon(Icons.confirmation_num),
+          SizedBox(height: 12),
+          Text(
+            "datos personales",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: primaryNavu,
+            ),
+          ),
+          SizedBox(height: 8),
+
+
+          Text(
+            "complete todos los campos ",
+            style: TextStyle(fontSize: 14, color: backgroundGray),
+          ),
+
+
+          SizedBox(height: 8,),
+
+
+          Text("Nombre",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: primaryNavu,
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'ingrese su nombre',
+            ),
+          ),
+
+
+
+        ],
+      ),
 
     ),
-    ],
     ),
-   );
+    );
+
   }
+}
